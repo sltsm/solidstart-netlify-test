444cbc103d65e93eec4a61a6f38aee7019c6d58b
Oliver Freese <sltsm@outlook.com>
2024-10-23 11:30:47 +0200

    feat: add server logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,18 @@
-import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
+import { cache, createAsync } from "@solidjs/router";
+
+const getData = async () => {
+  "use server";
+  console.log("Am I executed on the Server?");
+  return "I'm from far away...";
+};
 
 export default function Home() {
+  const data = createAsync(() => getData());
+
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
+    <main class="">
+      <h1 class="">Hello world!</h1>
+      <p>This is Content from the Server: {data()}</p>
     </main>
   );
 }
